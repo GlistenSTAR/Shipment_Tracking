1617c0f163a6d5f5580b0b18dc21ebec699a6a7c
GlistenStar <glistenstar00@gmail.com>
2021-02-18 22:01:18 +0200

    complete the tracking page

--- resources/views/Track/view.blade.php
+++ resources/views/Track/view.blade.php
@@ -8,7 +8,7 @@
           </div>
       </div>
   </x-slot>
-  <div class="py-12">
+  <div class="py-6">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mb-2 pb-2" align="center">
       <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
       <div class="col-md-12 pt-2">
@@ -18,7 +18,7 @@
               <span style="color:red;font-size:18px">* </span>Shipment Number : 
             </div>
             <div class="col-md-6 col-6 mb-2" style="width:100%" align="left">
-              <input type="text" class="form-control" id="shipmentnumber" name="shipmentnumber" style="border-radius:5px;" required>
+              <input type="text" class="form-control" value="321654987" id="shipmentnumber" name="shipmentnumber" style="border-radius:5px;" required>
             </div>
             <div class="col-md-2 col-3">
               <button class="btn btn-primary" autofocus>OK</button>
@@ -55,8 +55,8 @@
         for(var i in result){
           element += "<div style='border:1px solid grey;border-radius:10px;margin:20px'><i class='fa fa-barcode' style='font-size:100px;text-align:center'></i><h2 style='font-size:32px;color:blue;' align='center'>"+result[i]['shipmentnumber']+"</h2>\
             <div class='row'>\
-              <div class='col-md-6 pl-5' align='left' >\
-                <h4 align='left' class='pl-2' style='font-family:bold;font-size:40px; border-bottom:2px solid black'><span >Shipper info</span><h4>\
+              <div class='col-md-6 pl-5 pr-5' align='left' >\
+                <h4 align='left' class='pl-2 pr-2' style='font-family:bold;font-size:40px; border-bottom:2px solid black'><span >Shipper info</span><h4>\
                 <div class='row' style='font-size:20px;padding:5px;padding-top:10px;padding-bottom:10px'>\
                   <div class='col-md-4' align='left'><b>ShipperName : </b></div>\
                   <div class='col-md-8'>"
@@ -83,7 +83,7 @@
                 </div>\
               </div>\
               \
-              <div class='col-md-6' align='left'>\
+              <div class='col-md-6 pl-5 pr-5' align='left'>\
                 <h4 align='left' class='pl-2' style='font-family:bold;font-size:40px; border-bottom:2px solid black'><span >Receiver info</span><h4>\
                 <div class='row' style='font-size:20px;padding:5px;padding-top:10px;padding-bottom:10px'>\
                   <div class='col-md-4' align='left'><b>ReceiverName : </b></div>\
@@ -110,7 +110,66 @@
                   "</div>\
                 </div>\
               </div>\
-            </div></div>";
+            </div>\
+            <h4 align='left' class='pl-2 mt-3' style='font-family:bold;font-size:40px;margin-left:30px;margin-right:30px; border-bottom:2px solid black'><span >Shipment Information</span><h4>\
+            <div class='row'>\
+              <div class='col-md-12 pl-5' align='left' >\
+                <div class='row' style='font-size:20px;padding:5px;padding-top:10px;padding-bottom:10px'>\
+                  <div class='col-md-4' align='left'><b>Type of Shipperment : </b></div>\
+                  <div class='col-md-8'>"
+                      +result[i]['type']+
+                  "</div>\
+                </div>\
+                <div class='row' style='font-size:20px;padding:5px;padding-top:10px;padding-bottom:10px'>\
+                  <div class='col-md-4' align='left'><b>Courrier : </b></div>\
+                  <div class='col-md-8'>"
+                      +result[i]['courrier']+
+                  "</div>\
+                </div>\
+                <div class='row' style='font-size:20px;padding:5px;padding-top:10px;padding-bottom:10px'>\
+                  <div class='col-md-4' align='left'><b>Departure Time : </b></div>\
+                  <div class='col-md-8'>"
+                      +result[i]['departuretime']+
+                  "</div>\
+                </div>\
+                <div class='row' style='font-size:20px;padding:5px;padding-top:10px;padding-bottom:10px'>\
+                  <div class='col-md-4' align='left'><b>Origin : </b></div>\
+                  <div class='col-md-8'>"
+                      +result[i]['origin']+
+                  "</div>\
+                </div>\
+                <div class='row' style='font-size:20px;padding:5px;padding-top:10px;padding-bottom:10px'>\
+                  <div class='col-md-4' align='left'><b>Destination : </b></div>\
+                  <div class='col-md-8'>"
+                      +result[i]['destination']+
+                  "</div>\
+                </div>\
+                <div class='row' style='font-size:20px;padding:5px;padding-top:10px;padding-bottom:10px'>\
+                  <div class='col-md-4' align='left'><b>Pickup Date : </b></div>\
+                  <div class='col-md-8'>"
+                      +result[i]['pickupdate']+
+                  "</div>\
+                </div>\
+                <div class='row' style='font-size:20px;padding:5px;padding-top:10px;padding-bottom:10px'>\
+                  <div class='col-md-4' align='left'><b>Pickup Time : </b></div>\
+                  <div class='col-md-8'>"
+                      +result[i]['pickuptime']+
+                  "</div>\
+                </div>\
+                <div class='row' style='font-size:20px;padding:5px;padding-top:10px;padding-bottom:10px'>\
+                  <div class='col-md-4' align='left'><b>Expexted Dilvery Date : </b></div>\
+                  <div class='col-md-8'>"
+                      +result[i]['expectdate']+
+                  "</div>\
+                </div>\
+                <div class='row' style='font-size:20px;padding:5px;padding-top:10px;padding-bottom:10px'>\
+                  <div class='col-md-4' align='left'><b>Comments : </b></div>\
+                  <div class='col-md-8'>"
+                      +result[i]['commit']+
+                  "</div>\
+                </div>\
+              </div>\
+          </div>";
         }
         $('#newelement').html(element);
       }).catch(function(err){
